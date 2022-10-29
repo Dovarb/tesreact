cdd431013f07d1458216e5ef06704de2aa3bf1b6
Agung Jayasukma <jsp.agung@gmail.com>
2022-10-29 11:28:04 +0700

    Fix and clean up

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const MovieCard = ({ movie }) => {
+export default function MovieCard({ movie }) {
     return (
         <div className="movie">
             <div>
                 <p>{movie.Year}</p>
             </div>
-
             <div>
-                <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>
+                <img
+                    src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"}
+                    alt={movie.Title}
+                />
             </div>
-
             <div>
                 <span>{movie.Type}</span>
                 <h3>{movie.Title}</h3>
             </div>
         </div>
-    )
+    );
 }
-
-export default MovieCard;
